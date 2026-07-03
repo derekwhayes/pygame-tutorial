4c1e6a77cc14db36cdbeb6d276cefcde34b02cc4
Derek Hayes <derek2099@gmail.com>
2026-07-03 18:11:48 -0500

    Refactor player movement logic and add delta time handling

--- 5games-main/space shooter/code/main.py
+++ 5games-main/space shooter/code/main.py
@@ -8,6 +8,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 # plain surface
 surf = pygame.Surface((100, 200))
@@ -16,7 +17,8 @@ surf.fill('orange')
 # imports
 player_surf = pygame.image.load(join('5games-main/space shooter/images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = 1
+player_direction = pygame.math.Vector2(2, -1)
+player_speed = 300
 
 meteor_surf = pygame.image.load(join('5games-main/space shooter/images', 'meteor.png')).convert_alpha()
 meteor_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
@@ -30,6 +32,8 @@ star_surf = pygame.image.load(join('5games-main/space shooter/images', 'star.png
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
+    dt = clock.tick() / 1000
+
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,15 +46,25 @@ while running:
         display_surface.blit(star_surf, pos)
 
     display_surface.blit(laser_surf, laser_rect)
-
     display_surface.blit(meteor_surf, meteor_rect)
 
-    player_rect.left += player_direction * 0.4
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_direction *= -1
-
+    # player movement
+    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surf, player_rect)
 
+    if player_rect.left < 0:
+        player_direction.x *= -1
+        player_rect.left = 0
+    if player_rect.right > WINDOW_WIDTH:
+        player_direction.x *= -1
+        player_rect.right = WINDOW_WIDTH
+    if player_rect.top < 0:
+        player_direction.y *= -1
+        player_rect.top = 0;
+    if player_rect.bottom > WINDOW_HEIGHT:
+        player_direction.y *= -1
+        player_rect.bottom = WINDOW_HEIGHT
+    
     pygame.display.update()
 
 pygame.quit()
