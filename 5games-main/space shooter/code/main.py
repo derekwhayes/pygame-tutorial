f4940dbd7df818befda4d0a11ee0282639b00613
Derek Hayes <derek2099@gmail.com>
2026-07-05 17:25:18 -0500

    Implement player shooting mechanics and refactor player movement logic

--- 5games-main/space shooter/code/main.py
+++ 5games-main/space shooter/code/main.py
@@ -2,6 +2,69 @@ import pygame
 from random import randint
 from os.path import join
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('5games-main/space shooter/images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.math.Vector2()
+        self.speed = 300
+
+        # cooldown
+        self.can_shoot = True
+        self.laser_shoot_time = 0
+        self.cooldown_duration = 400
+    
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
+            Laser(laser_surf, self.rect.midtop, all_sprites)
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(midbottom = pos)
+
+    def update(self, dt):
+        self.rect.centery -= 400 * dt
+        if self.rect.bottom < 0:
+            self.kill()
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = pos)
+        self.lifetime = 3000
+        self.start_time = pygame.time.get_ticks()
+
+    def update(self, dt):
+        self.rect.centery += 400 * dt
+
+        if pygame.time.get_ticks() - self.start_time >= self.lifetime:
+            self.kill()
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -10,29 +73,20 @@ pygame.display.set_caption('Space Shooter')
 running = True
 clock = pygame.time.Clock()
 
-
-
-
-# plain surface
-surf = pygame.Surface((100, 200))
-surf.fill('orange')
-
-# imports
-player_surf = pygame.image.load(join('5games-main/space shooter/images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2()
-player_speed = 300
-
-meteor_surf = pygame.image.load(join('5games-main/space shooter/images', 'meteor.png')).convert_alpha()
-meteor_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-
-laser_surf = pygame.image.load(join('5games-main/space shooter/images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
-
+# import
 star_surf = pygame.image.load(join('5games-main/space shooter/images', 'star.png')).convert_alpha()
+laser_surf = pygame.image.load(join('5games-main/space shooter/images', 'laser.png')).convert_alpha()
+meteor_surf = pygame.image.load(join('5games-main/space shooter/images', 'meteor.png')).convert_alpha()
 
-# random star coordinates
-star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+# sprites
+all_sprites = pygame.sprite.Group()
+for i in range(20):
+    Star(all_sprites, star_surf)
+player = Player(all_sprites)
+
+# custom events -> meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 400)
 
 while running:
     dt = clock.tick() / 1000
@@ -41,44 +95,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == meteor_event:
+            Meteor(meteor_surf, (randint(0, WINDOW_WIDTH), randint(-200, -100)), all_sprites)
 
-    # input
-    keys = pygame.key.get_pressed()
-    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-    player_direction = player_direction.normalize() if player_direction else player_direction
-    player_rect.center += player_direction * player_speed * dt
-
-    recent_keys = pygame.key.get_just_pressed()
-    if recent_keys[pygame.K_SPACE]:
-        print("fire laser")
+    # update
+    all_sprites.update(dt)
+    player.laser_timer()
 
     # draw the game
     display_surface.fill('darkgray')
-
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
-
-    display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(meteor_surf, meteor_rect)
-
-    # player movement
-    player_rect.center += player_direction * player_speed * dt
-    display_surface.blit(player_surf, player_rect)
-
-    if player_rect.left < 0:
-        player_direction.x *= -1
-        player_rect.left = 0
-    if player_rect.right > WINDOW_WIDTH:
-        player_direction.x *= -1
-        player_rect.right = WINDOW_WIDTH
-    if player_rect.top < 0:
-        player_direction.y *= -1
-        player_rect.top = 0;
-    if player_rect.bottom > WINDOW_HEIGHT:
-        player_direction.y *= -1
-        player_rect.bottom = WINDOW_HEIGHT
-    
+    all_sprites.draw(display_surface)
     pygame.display.update()
 
 pygame.quit()
